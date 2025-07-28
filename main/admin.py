4f9233c5df55230a266bc26e6ea12b9e19dba7e6
marpancel <pancmarcel@gmail.com>
2025-07-28 15:47:24 +0200

    major update

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Project, ProjectImage
 
 # Register your models here.
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category')
+    inlines = [ProjectImageInline]
+
+admin.site.register(Category)
+admin.site.register(Project, ProjectAdmin)
